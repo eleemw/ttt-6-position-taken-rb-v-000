2f5ef17866e92cbb936eb6eeab28e8976e7d2e6b
Erica Murdock-Waters <ericahpa@gmail.com>
2019-10-07 15:29:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -12,7 +12,7 @@
 # end
 
 def position_taken?(board, index)
-  if board[index] != (" ") || board[index] != ("")
+  if (board[index] != (" ")) || (board[index] != ("")) || (board[index] != (nil))
     true
   else
     false
